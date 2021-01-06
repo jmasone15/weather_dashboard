a236dbc5a9b3f0b30c1de0310884a38acfb60ad7
Jordan Masone <jordanmasone1@gmail.com>
2021-01-06 17:25:02 -0500

    Initial commit.

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    $("#search-button").on("click", function() {
+        var searchValue = $("#search-value").val()
+        console.log(searchValue)
+        $("#search-value").val("")
+
+        searchWeather(searchValue);
+    })
+
+    let APIkey = "ed8b97e489f3133d4b5f41f0db8eaa8e";
+
+    function searchWeather(searchValue) {
+        $.ajax({
+            type: "GET",
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIkey,
+            dataType: "json",
+            success: function(data) {
+                console.log(data);
+            }
+        })
+    }
+
+    
+    // api.openweathermap.org/data/2.5/weather?q=&appid=
+})
