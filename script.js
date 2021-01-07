0386ed0aba7629aec11109d762ca38d851be6715
Jordan Masone <jordanmasone1@gmail.com>
2021-01-06 19:20:06 -0500

    Added console.log's for desired weather values.

--- script.js
+++ script.js
@@ -16,9 +16,32 @@ $(document).ready(function(){
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIkey,
             dataType: "json",
             success: function(data) {
+                
+                let tempF = ((data.main.temp - 273.15) * 1.80 + 32).toFixed(1);
+                
                 console.log(data);
+                console.log("Temperature (F): " + tempF);
+                console.log("Humidity: " + data.main.humidity);
+                console.log("Wind Speed: " + data.wind.speed);
+                console.log("Latitude, Longitude: " + data.coord.lat + ", " + data.coord.lon);
+                let lattitude = data.coord.lat
+                let longitude = data.coord.lon
+
+                $.ajax({
+                    type: "GET",
+                    url: "https://api.openweathermap.org/data/2.5/uvi?lat=" + lattitude + "&lon=" + longitude + "&appid=" + APIkey,
+                    dataType: "json",
+                    success: function(data) {
+                        
+                        console.log(data);
+                        console.log("UV Index: " + data.value);
+                    }
+                })
             }
         })
+
+        
+        
     }
 
 })
